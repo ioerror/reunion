4c3dc5fe943e224e2a4fcbe06b9f4b51f5505dd0
Reunion Author 1 <reunion1@rendezvous.contact>
2024-08-17 17:39:53 +0200

    Import by name rather than by wildcard

--- test/test_primitives.py
+++ test/test_primitives.py
@@ -1,8 +1,10 @@
 import os
 import unittest
 
-from reunion.primitives import *
-
+from reunion.primitives import aead_decrypt, aead_encrypt, argon2i
+from reunion.primitives import generate_hidden_key_pair, Hash
+from reunion.primitives import highctidh_deterministic_rng, hkdf, prp_decrypt
+from reunion.primitives import prp_encrypt, unelligator, x25519
 
 class TestPrimitives(unittest.TestCase):
     def test_aead(self):
